b86c37e1adefc26503a03c52e5337475bc5e8c17
Yash Srivastav <admin@yashsriv.org>
2017-03-14 20:30:28 +0530

    Add Improvements

--- frontend/src/app/student.component.ts
+++ frontend/src/app/student.component.ts
@@ -19,8 +19,8 @@ export class StudentComponent {
   constructor(private sanitizer: DomSanitizer) {}
 
   url = () => {
-    let generic = this.student.g === 'F' ? 'https://fbcdn-profile-a.akamaihd.net/static-ak/rsrc.php/v2/yp/r/yDnr5YfbJCH.gif' : 'https://fbcdn-profile-a.akamaihd.net/static-ak/rsrc.php/v2/yL/r/HsTZSDw4avx.gif';
-    return this.sanitizer.bypassSecurityTrustStyle(`url("http://home.iitk.ac.in/~${this.student.u}/dp"), url("http://oa.cc.iitk.ac.in:8181/Oa/Jsp/Photo/${this.student.i}_0.jpg"), url("${generic}")`);
+    const generic = this.student.g === 'F' ? 'https://fbcdn-profile-a.akamaihd.net/static-ak/rsrc.php/v2/yp/r/yDnr5YfbJCH.gif' : 'https://fbcdn-profile-a.akamaihd.net/static-ak/rsrc.php/v2/yL/r/HsTZSDw4avx.gif';
+    return this.sanitizer.bypassSecurityTrustStyle(`url("http://home.iitk.ac.in/~${this.student.u}/dp"), url("/Oa/Jsp/Photo/${this.student.i}_0.jpg"), url("${generic}")`);
   }
 
 }
