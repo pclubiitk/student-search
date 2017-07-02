7eb0e1ef5fac21a338d1bab81772b2793c86ad28
Yash Srivastav <admin@yashsriv.org>
2017-07-03 02:04:09 +0530

    Updated for Angular 4

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { ClipboardModule } from 'ngx-clipboard';
-import { InfiniteScrollModule } from 'angular2-infinite-scroll';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { MasonryModule } from 'angular2-masonry';
 
 import { DetailComponent } from './components/detail';
@@ -30,6 +31,7 @@ import { StudentComponent } from './components/student';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     ClipboardModule,
     FormsModule,
     HttpModule,
