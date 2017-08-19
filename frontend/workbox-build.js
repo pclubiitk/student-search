b6f7904e05e271911d4fa41e2c8c14068234b7fb
Yash Srivastav <admin@yashsriv.org>
2017-08-19 09:24:02 +0530

    Convert to a PWA for better accessibility

---
+++ frontend/workbox-build.js
@@ -0,0 +1,24 @@
+const swBuild = require('workbox-build');
+
+swBuild.generateSW({
+  navigateFallback: 'index.html',
+  globDirectory: './dist',
+  globPatterns: [
+    'index.html',
+    '**.js',
+    '**.css'
+  ],
+  swDest: 'dist/service-worker.js',
+  runtimeCaching: [
+    {
+      urlPattern: /\/api\/students\//,
+      handler: 'networkFirst',
+      options: {
+        cacheName: 'api-cache',
+        cacheExpiration: {
+          maxEntries: 10
+        }
+      }
+    }
+  ]
+}).then(() => console.log('Service Worker generated')).catch(err => console.error(err, 'Service Worker failed to generate'));
