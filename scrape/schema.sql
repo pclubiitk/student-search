8da7f62cf2983a9b0c257baf2eec9b1e420f14ef
Yash Srivastav <admin@yashsriv.org>
2018-07-24 13:04:40 +0530

    Update scraping script as per new oa session-based system

---
+++ scrape/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS students(
+       roll TEXT PRIMARY KEY,
+       username TEXT,
+       name TEXT,
+       program TEXT,
+       dept TEXT,
+       hall TEXT,
+       room TEXT,
+       blood_group TEXT,
+       gender TEXT,
+       hometown TEXT
+);
---
+++ scrape/scrape.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import requests
+import sqlite3
+conn = sqlite3.connect('students.db')
+c = conn.cursor()
+
+s = requests.Session()
+s.get("https://oa.cc.iitk.ac.in/Oa/Jsp/Main_Frameset.jsp")
+s.get("https://oa.cc.iitk.ac.in/Oa/Jsp/Main_Intro.jsp?frm='SRCH'")
+s.get("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITK_Srch.jsp?typ=stud")
+
+headers = {
+    "Referer": "https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITK_Srch.jsp?typ=stud"
+}
+
+headers1 = {
+    "Referer": "https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchStudRoll_new.jsp"
+}
+
+payload = {
+    'k4': 'oa',
+    'numtxt': '',
+    'recpos': 0,
+    'str': '',
+    'selstudrol': '',
+    'selstuddep': '',
+    'selstudnam': '',
+    'txrollno': '',
+    'Dept_Stud': '',
+    'selnam1': '',
+    'mail': ''
+}
+
+payload1 = {
+    'typ': ['stud'] * 12,
+    'numtxt': '',
+    'sbm': ['Y'] * 12
+}
+
+TOTAL = 8385
+
+for i in range(0, TOTAL+1, 12):
+    payload['recpos'] = i
+    r = s.post("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchStudRoll_new.jsp", headers=headers, data=payload)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    for link in soup.select('.TableText a'):
+        roll = link.get_text().strip()
+        payload1['numtxt'] = roll
+        r1 = s.post("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchRes_new.jsp", headers=headers1, data=payload1)
+        soup1 = BeautifulSoup(r1.text, 'html.parser')
+
+        name = ''
+        program = ''
+        dept = ''
+        hall = ''
+        room = ''
+        username = ''
+        blood_group = ''
+        gender = ''
+        hometown = ''
+
+        for para in soup1.select('.TableContent p'):
+            body = para.get_text().strip()
+            field = body.split(':')
+            key = field[0].strip()
+            value = field[1].strip()
+            if key == 'Name':
+                name = value.lower().title()
+            elif key == 'Program':
+                program = value
+            elif key == 'Department':
+                dept = value.lower().title()
+            elif key == 'Hostel Info':
+                if len(value.split(',')) > 1:
+                    hall = value.split(',')[0].strip()
+                    room = value.split(',')[1].strip()
+            elif key == 'E-Mail':
+                if len(value.split('@')) > 1:
+                    username = value.split('@')[0].strip()
+            elif key == 'Blood Group':
+                blood_group = value
+            elif key == 'Gender':
+                if len(value.split('\t')) > 1:
+                    gender = value.split('\t')[0].strip()
+            else:
+                print("{} {}".format(key, value))
+
+        body = soup1.prettify()
+        if len(body.split('Permanent Address :')) > 1:
+            address = body.split('Permanent Address :')[1].split(',')
+            if len(address) > 2:
+                address = address[len(address) - 3: len(address) - 1]
+                hometown = "{}, {}".format(address[0], address[1])
+
+        c.execute('REPLACE INTO students VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                  (roll, username, name, program, dept, hall, room,
+                   blood_group, gender, hometown))
+
+    print("Processed {}".format(i + 12))
+    conn.commit()
+conn.close()
