4cf99101e6600f4cdf6ff4e605b60b3208b43107
Bhagwat Garg <bhagwatgarg10@gmail.com>
2021-02-09 02:37:19 +0530

    Updated frontend to support reverse image search

--- frontend/src/app/components/image-upload/image-dialog.component.ts
+++ frontend/src/app/components/image-upload/image-dialog.component.ts
@@ -1,6 +1,10 @@
 import {Component, Inject} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+import { DetailComponent } from '../detail';
+import { Student } from '../../models/student.model';
 import { ImageService } from './image.service';
 
 export interface DialogData {
@@ -41,12 +45,15 @@ export class ImageDialogButtonComponent {
 export class ImageDialogComponent {
 
   defaultURI='https://www.jamiemaison.com/creating-a-simple-text-editor/placeholder.png'
+  success=false
   preview=this.defaultURI
   toUpload=null
   tried=false
   inIITK=false
 
   constructor(
+    private snackBar: MatSnackBar,
+    public dialog: MatDialog,
     public dialogRef: MatDialogRef<ImageDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData, public serv: ImageService) {
   }
@@ -58,35 +65,48 @@ export class ImageDialogComponent {
     let f=<File>e.target.files[0];
     if(f.type.split('/')[0]!='image'){
       // console.log(f.type);
-      //TODO snackbar
+      this.snackBar.open("Non-image files are not supported", "CLOSE", {duration: 2500});
       return
     }
     if(f.size>10000000){
       console.log(f.size);
-      console.log('File too big');
+      this.snackBar.open("Image size should be < 10MB", "CLOSE", {duration: 2500});
       return;
     }
     this.toUpload=f;
     var reader = new FileReader();
     reader.onload = (event: any) => {
       this.preview = event.target.result;
-      // console.log(this.preview)
     };
     reader.onerror = (event: any) => {
       console.log("File could not be read: " + event.target.error.code);
     };
     reader.readAsDataURL(this.toUpload);
 
-    // console.log(this.toUpload);
   }
 
-  onSubmit(form: NgForm){
+  async onSubmit(form: NgForm){
     // console.log(form.form.value);
     if(form.form.valid&&this.inIITK){
-      this.serv.postFile(this.toUpload, parseInt(form.form.value.radio));
+      this.success=true;
+      let res=await this.serv.postFile(this.toUpload, parseInt(form.form.value.radio)).catch((err)=>this.success=false);
+      if(!this.success){
+        this.snackBar.open("Something went wrong", "CLOSE", {duration: 2500});
+        return;
+      }
+      this.dialog.open(DetailComponent, {
+        data: {
+          student: res
+        },
+        panelClass: 'centered-dialog',
+        width: '360px'
+      });
     }
     else{
       this.tried=true
+      if(!this.inIITK){
+      this.snackBar.open("Connect to IITK server", "CLOSE", {duration: 2500});
+      }
     }
   }
 
--- frontend/src/app/components/image-upload/image.service.ts
+++ frontend/src/app/components/image-upload/image.service.ts
@@ -5,16 +5,21 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ImageService {
+  res={};
   constructor(private http: HttpClient) {}
 
-  postFile(fileToUpload: File, radio) {
+  async postFile(fileToUpload: File, radio) {
     const endpoint = '/api/image-upload';
     const formData: FormData = new FormData();
     formData.append('image', fileToUpload, fileToUpload.name);
     formData.append('type', radio===1?'Person':'Landmark');
     // console.log(formData.get('type'));
-    this.http.post(endpoint, formData).subscribe(res=>{
-      console.log(res);
-    });
+    this.res=await this.http.post(endpoint, formData).toPromise();
+
+    //await this.http.post(endpoint, formData).subscribe(res=>{
+    //  console.log(res);
+    //  this.res=res;
+    //});
+    return this.res;
   }
 }
